8dbed9dd810b6723c764b0f16d911c3657d60be8
Aphaki <kiapha1108@gmail.com>
2022-04-21 15:33:57 +0900

    Initial Commit

---
+++ LATime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LATime
+//
+//  Created by Sy Lee on 2022/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LATime/LATimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LATimeApp.swift
+//  LATime
+//
+//  Created by Sy Lee on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LATimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
